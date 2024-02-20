a352e6dce48e50cf2897b0b6bdeae1b52f7078e0
pedrosantanacoder <158317146+pedrosantanacoder@users.noreply.github.com>
2024-02-20 10:34:41 -0300

    Delete app/ready directory

--- app/ready/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Finished from '../../components/Finished'
-import '../../styles/global.css'
-
-export default function Ready() {
-  return (
-    <Finished />
-  )
-}
