e3a9268127977bd9b2e217423f04d29821b2d201
pedrosantanacoder <158317146+pedrosantanacoder@users.noreply.github.com>
2024-02-20 16:29:36 -0300

    Update payment.ts

--- pages/api/payment.ts
+++ pages/api/payment.ts
@@ -5,7 +5,7 @@ type ResponseData = {
 }
 
 function numberRandomizer() {
-  return Math.floor(Math.random() * (25905 - 25900 + 1)) + 25900
+  return Math.floor(Math.random() * (25950 - 25900 + 1)) + 25900
 }
 
 export default async function handler(
